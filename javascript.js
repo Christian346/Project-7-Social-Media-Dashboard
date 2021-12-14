63329fc3679dc6c38bf3496633ff2d78eb9491c2
Christian <chris.346@hotmail.com>
2021-12-14 00:12:54 -0500

    triangles through js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,5 @@
-let facebookData = ['@nathan','1987','FOLLOWERS','12']
 
-
-
-
-
-
+//variables for color change and appends of data
 let body = document.querySelector('body');
 let header = document.querySelector(".header")
 let secondSec = document.querySelector(".second-sec");
@@ -16,7 +11,66 @@ let toggleButton = document.querySelector("#toggler");
 
 let togglecheck = document.querySelector("#toggler").checked;//chebox value true
 //function that turns dark mode
-console.log(togglecheck)
+//console.log(togglecheck)
+
+//--data variables below
+
+//upperboxes classes
+let upperboxesclasses =['facebook-main','twit-box' ,'ig-box','yout-box']
+
+//upper boxes data
+let facebookData = ['@nathan', '1987','FOLLOWERS', 12]
+let twitterData =['@nathan', '1044','FOLLOWERS',99]
+let igData = ['@nathan','11k','FOLLOWERS',1099]
+let youtubeData =['@nathan','8239','SUBSCRIBERS',-144]
+//lower boxes data
+let fbLowData1= ['87', 3]
+let fbLowData2= ['52', -2]
+
+let igLowData1 =['5462', 2257]
+let igLowData2=['52k', 1375]
+
+let twitterLowData1 =['117',303]
+let twitterLowData2 =['507',553]
+
+let youtubeLowData1 = ['107',-19]
+let youtubeLowData2 = ['1407',-12]
+
+//facebook box 
+    //where info will be appended
+    let upBoxes = document.querySelector(".up-boxes")
+
+    //create content variable
+    let content = `
+    <div class="upper-part-header-boxes">
+            <i><img src="images/icon-facebook.svg" alt=""></i> <p>${facebookData[0]}</p>
+          </div>
+          <p class="up-boxes-p1">${facebookData[1]}</p>
+          <p class="up-boxes-p2">${facebookData[2]}</p>
+          
+          <div class="results-flex">
+          <div class="arrows"> <div class ="triangle-up"></div> <div class="triangle-down"></div></div>
+          <p class="up-boxes-p3">${Math.abs(facebookData[3])} today</p>
+        </div>
+        </div>
+
+    `;
+    //make the content go into the div
+    upBoxes.innerHTML = content;
+    //
+    if(facebookData[3] > 0){
+     document.querySelector('.triangle-up').style.visibility = "visible"
+    }
+    else{
+        document.querySelector('.triangle-down').style.visibility = "visible"
+    }
+//facebook box ends 
+    
+ 
+    
+
+
+//function that turns dark mode
 function turnColorToBlack(){
    
     if(togglecheck == false){
