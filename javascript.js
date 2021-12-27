bb97a5304e1ecf29ac697104b6a5439610a60d4b
Christian <chris.346@hotmail.com>
2021-12-27 14:50:51 -0500

    attempt to consume data 3

--- javascript.js
+++ javascript.js
@@ -50,53 +50,124 @@ fetch(`${api_Url}/Companies`)
 
 
 //2 boxes each
-for(var i =0;i < companies1[0].overview.length ;i++){
-let lowBoxes = document.querySelector('.box-1')
-let ContentDiv2 = document.createElement('div')
+ 
+   
+    //console.log(companies1[0].overview.length)//2 overview
+    
+ 
+ 
 
-//second map
-let tpl2 = companies1.map( lowbox => 
-    `
-   <div class="sec-box-dark sec-box ">
-        <div class="box-1-upper">
-            <p class="box-1-upper-p">${lowbox.overview[i].upperText}</p>
-            <i class="box-1-upper-i"><img src="images/${lowbox.overview[i].lowIcon}" alt=""></i>
-        </div>
+    let lowBoxes = document.querySelector('.box-1')
+    let ContentDiv2 = document.createElement('div')
 
-        <div class="box-1-lower">
-            <p class="box-1-lower-p">${lowbox.overview[i].lowerTextValue}</p>
+    let tpl2 = companies1.map( item => 
+            `
+        <div class="sec-box-dark sec-box ">
+                <div class="box-1-upper">
+                    <p class="box-1-upper-p">${item.overview[0].upperText}</p>
+                    <i class="box-1-upper-i"><img src="images/${item.overview[0].lowIcon}" alt=""></i>
+                </div>
 
-            <div class="results-flex-2">
-                <div class="arrows-2"><div class ="triangle-up2"></div> <div class="triangle-down2"></div></div>
-                <p class="box-1-lower-p2">${Math.abs(lowbox.overview[i].percentage)} %</p>
+                <div class="box-1-lower">
+                    <p class="box-1-lower-p">${item.overview[0].lowerTextValue}</p>
+
+                    <div class="results-flex-2">
+                        <div class="arrows-2"><div class ="triangle-up2"></div> <div class="triangle-down2"></div></div>
+                        <p class="box-1-lower-p2">${Math.abs(item.overview[0].percentage)} %</p>
+                    </div>
+
+                </div>
             </div>
 
-        </div>
-    </div>
+            <div class="sec-box-dark sec-box ">
+                <div class="box-1-upper">
+                    <p class="box-1-upper-p">${item.overview[1].upperText}</p>
+                    <i class="box-1-upper-i"><img src="images/${item.overview[1].lowIcon}" alt=""></i>
+                </div>
 
-    `
+                <div class="box-1-lower">
+                    <p class="box-1-lower-p">${item.overview[1].lowerTextValue}</p>
+
+                    <div class="results-flex-2">
+                        <div class="arrows-2"><div class ="triangle-up2"></div> <div class="triangle-down2"></div></div>
+                        <p class="box-1-lower-p2">${Math.abs(item.overview[1].percentage)} %</p>
+                    </div>
+
+                </div>
+            </div>
+
+            `
 ).join('')
 ContentDiv2.classList.add('super-flex')
 ContentDiv2.innerHTML = tpl2;
 lowBoxes.append(ContentDiv2);
+
+    
+                     //4           //2
+//console.log(companies1.overview)
+//console.log(companies1[0].overview[0].percentage)
+
+for(var i = 0; i < companies1.length;i++){
+    
+    for(var j=0; j < companies1[i].overview.length; j++){
+             
+        if( companies1[i].overview[j].percentage > 0 ){
+            //manera de cambiar entre objetos buscar!!!!
+            document.querySelectorAll('.triangle-up2')[i].style.visibility = "visible"
+            document.querySelectorAll('.box-1-lower-p2')[i].style.color ="hsl(163, 72%, 41%)"
+           }
+           else{
+            document.querySelectorAll('.triangle-down2')[i].style.visibility = "visible"
+            document.querySelectorAll('.box-1-lower-p2')[i].style.color ="hsl(356, 69%, 56%)"
+           }
+        
+            console.log(companies1[i].overview[j].percentage)
+    }
+  
 }
-//?
-//validation for each 2 boxes
-for(var i =0; i < companies1.length; i++){
+//
 
-    if(companies1[i].overview[i].percentage > 0 ){
 
-        document.querySelectorAll('.triangle-up2')[i].style.visibility = "visible"
-        document.querySelectorAll('.box-1-lower-p2')[i].style.color ="hsl(163, 72%, 41%)"
-       }
-       else{
-        document.querySelectorAll('.triangle-down2')[i].style.visibility = "visible"
-        document.querySelectorAll('.box-1-lower-p2')[i].style.color ="hsl(356, 69%, 56%)"
-       }
+//variables for color change and appends of data
+let body = document.querySelector('body');
+let header = document.querySelector(".header")
+let secondSec = document.querySelector(".second-sec");
+let upBox = document.querySelectorAll(".up-boxes");//remember is a list of classes will need for each loop to iterate through all of em
+let secoverp = document.querySelector(".sec-over-p");
+let boxone = document.querySelector(".box-1");
+let secBox = document.querySelectorAll(".sec-box");//remember is a list of classes will need for each loop to iterate through all of em
+let toggleButton = document.querySelector("#toggler");
+//
+let togglecheck = document.querySelector("#toggler").checked;//chebox value true
+//function that turns dark mode
+//console.log(togglecheck)
 
-       
-   }
+
+//function that turns dark mode
+function turnColorToBlack(){
    
+    if(togglecheck == false){
+
+    body.classList.toggle("body-dark")
+    header.classList.toggle("header-dark")
+    secondSec.classList.toggle("second-sec-dark")
+ 
+    upBox.forEach(item =>{
+    item.classList.toggle("up-boxes-dark")
+    })
+ 
+    secoverp.classList.toggle("sec-over-p-dark")
+    boxone.classList.toggle("box-1-dark")
+    
+    secBox.forEach(item =>{
+    item.classList.toggle("sec-box-dark")
+    })
+   }
+}
+
+//addeventListener to the darkmode button 
+
+toggleButton.addEventListener("click", turnColorToBlack)
 
 })
 
@@ -208,43 +279,3 @@ if(secondJson[i].percentage > 0 )
 */
 //!selection of variables after renderization
 
-//variables for color change and appends of data
-let body = document.querySelector('body');
-let header = document.querySelector(".header")
-let secondSec = document.querySelector(".second-sec");
-let upBox = document.querySelectorAll(".up-boxes");//remember is a list of classes will need for each loop to iterate through all of em
-let secoverp = document.querySelector(".sec-over-p");
-let boxone = document.querySelector(".box-1");
-let secBox = document.querySelectorAll(".sec-box");//remember is a list of classes will need for each loop to iterate through all of em
-let toggleButton = document.querySelector("#toggler");
-//
-let togglecheck = document.querySelector("#toggler").checked;//chebox value true
-//function that turns dark mode
-//console.log(togglecheck)
-
-
-//function that turns dark mode
-function turnColorToBlack(){
-   
-    if(togglecheck == false){
-
-    body.classList.toggle("body-dark")
-    header.classList.toggle("header-dark")
-    secondSec.classList.toggle("second-sec-dark")
- 
-    upBox.forEach(item =>{
-    item.classList.toggle("up-boxes-dark")
-    })
- 
-    secoverp.classList.toggle("sec-over-p-dark")
-    boxone.classList.toggle("box-1-dark")
-    
-    secBox.forEach(item =>{
-    item.classList.toggle("sec-box-dark")
-    })
-   }
-}
-
-//addeventListener to the darkmode button 
-
-toggleButton.addEventListener("click", turnColorToBlack)
